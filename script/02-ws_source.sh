1c197e7705b32befc55ccea4a3ead532e566b7f9
teo3300 <matteo.rogora@live.it>
2022-04-02 15:11:13 +0200

    fixed ws_source

--- script/02-ws_source.sh
+++ script/02-ws_source.sh
@@ -6,6 +6,7 @@ base="$HOME/git/robotics-2022"
 
 if [ "$base/src" != "$(echo $ROS_PACKAGE_PATH | cut -d':' -f1)" ]; then
 	echo "Sourcing workspace..."
+    source devel/setup.bash
 fi
 
 echo "DONE!"
