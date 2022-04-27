7b6a057923df65593c7615a0d4f9d147379c2771
Davide Grazzani <davide.grazzani@mail.polimi.it>
2022-04-27 11:10:39 +0200

    finito nodo inverso

--- src/inverse_rpm/src/inverse_rpm_calculator.cpp
+++ src/inverse_rpm/src/inverse_rpm_calculator.cpp
@@ -3,22 +3,32 @@
 #include "geometry_msgs/Pose.h"
 #include "nav_msgs/Odometry.h"
 
-#include "../../geometry/include/geometry/odometry.hpp"
+#include "geometry/odometry.hpp"
 #include "../../proj_const/include/proj_const/constants.hpp"
 #include "inverse_rpm/Wheels_Rpm.h"
 
 Matrix inverse_matrix(4,3);
 Matrix result(4,1);
+ros::Subscriber sub;
+ros::Publisher pub;
 
 void inverseKinCallBack(const nav_msgs::Odometry::ConstPtr& msg){
+    //getting values from topic odom
     double* values = new double(3);
     values[0] = msg->pose.pose.position.x;
     values[1] = msg->pose.pose.position.y;
     values[2] = msg->pose.pose.position.z;
     Matrix pose(3,1,values);
+    //performing inverse computation
     result = inverse_matrix * pose;
     inverse_rpm::Wheels_Rpm wheel_msg;
-    //creazione del messaggio da finire
+    //genererating and sending msg
+    wheel_msg.rpm_fl=result.get(0,0);
+    wheel_msg.rpm_fr=result.get(1,0);
+    wheel_msg.rpm_rl=result.get(2,0);
+    wheel_msg.rpm_rr=result.get(3,0);
+    pub.publish(wheel_msg);
+    free(values);
 }
 
 int main(int argc, char *argv[]){
@@ -31,8 +41,8 @@ int main(int argc, char *argv[]){
     inverse_matrix.fill(inverse_matrix_values);
 
 
-    ros::Subscriber sub = node_Handle.subscribe("odom",1000,inverseKinCallBack);
-    ros::Publisher pub = node_Handle.advertise<inverse_rpm::Wheels_Rpm>("wheels_rpm",1000);
+    sub = node_Handle.subscribe("odom",1000,inverseKinCallBack);
+    pub = node_Handle.advertise<inverse_rpm::Wheels_Rpm>("wheels_rpm",1000);
 
     ros::spin();
 
