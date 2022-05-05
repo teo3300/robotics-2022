3b2a6be5f3d7af9795c32ea59237dc4c7653ccab
Davide Grazzani <davide.grazzani@mail.polimi.it>
2022-05-05 16:46:50 +0200

    inverse_kin_fixed

--- src/inverse_rpm/src/inverse_rpm_calculator.cpp
+++ src/inverse_rpm/src/inverse_rpm_calculator.cpp
@@ -2,7 +2,7 @@
 #include "ros/publisher.h"
 #include "ros/ros.h"
 #include "geometry_msgs/Pose.h"
-#include "nav_msgs/Odometry.h"
+#include "geometry_msgs/TwistStamped.h"
 #include <sensor_msgs/JointState.h>
 
 #include "geometry/odometry.hpp"
@@ -15,17 +15,18 @@ double clean[4] = {0,0,0,0};
 
 Matrix inverse_matrix(4,3);
 Matrix result(4,1,clean);
+
 ros::Subscriber sub;
 ros::Publisher pub;
 ros::Subscriber bag;
 ros::Publisher bagpub;
 
-void inverseKinCallBack(const nav_msgs::Odometry::ConstPtr& msg){
+void inverseKinCallBack(const geometry_msgs::TwistStamped::ConstPtr& msg){
     //getting values from topic odom
     double* values = new double(3);
-    values[0] = msg->pose.pose.position.x;
-    values[1] = msg->pose.pose.position.y;
-    values[2] = msg->pose.pose.position.z;
+    values[0] = msg->twist.linear.x;
+    values[1] = msg->twist.linear.y;
+    values[2] = msg->twist.angular.z;
     Matrix pose(3,1,values);
     //performing inverse computation
     result = inverse_matrix * pose;
@@ -45,18 +46,6 @@ void inverseKinCallBack(const nav_msgs::Odometry::ConstPtr& msg){
     result.fill(clean);
 }
 
-void bagCallBack(const sensor_msgs::JointState::ConstPtr& msg){
-    inverse_rpm::Wheels_Rpm wheel_msg;
-    wheel_msg.rpm_fl=msg->velocity[0];
-    wheel_msg.rpm_fr=msg->velocity[1];
-    wheel_msg.rpm_rl=msg->velocity[2];
-    wheel_msg.rpm_rr=msg->velocity[3];
-    wheel_msg.header.stamp = msg->header.stamp;
-    wheel_msg.header.frame_id = msg->header.frame_id;
-    wheel_msg.header.seq = msg->header.seq;
-    bagpub.publish(wheel_msg);
-}
-
 int main(int argc, char *argv[]){
 
     ros::init(argc, argv,"inverse_kinematic_calculator");
@@ -71,11 +60,9 @@ int main(int argc, char *argv[]){
    
     inverse_matrix.fill(inverse_matrix_values);
    
-    sub = node_Handle.subscribe("odom",1000,inverseKinCallBack);
+    sub = node_Handle.subscribe("cmd_vel",1000,inverseKinCallBack);
     pub = node_Handle.advertise<inverse_rpm::Wheels_Rpm>("wheels_rpm",1000);
-    bag = node_Handle.subscribe("wheel_states",1000,bagCallBack);
-    bagpub = node_Handle.advertise<inverse_rpm::Wheels_Rpm>("bag_read_rpm",1000);
-
+    
     ros::spin();
 
     return 0;
